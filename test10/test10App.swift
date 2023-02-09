e9f64e40caa83fd71905d8eb1decb02762268533
malak <malakhulimi@gmail.com>
2023-02-09 17:34:34 +0300

    Initial Commit

---
+++ test10/test10App.swift
@@ -0,0 +1,17 @@
+//
+//  test10App.swift
+//  test10
+//
+//  Created by lamya on 18/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct test10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
